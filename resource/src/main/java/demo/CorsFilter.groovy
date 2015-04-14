7703ed3cc8ceaa8b5cce78a46c8fb084d2e70aa3
pstinghua <pstinghua@gmail.com>
2015-04-14 18:04:52 +0800

    updated

---
+++ resource/src/main/java/demo/CorsFilter.groovy
@@ -0,0 +1,47 @@
+package demo
+
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
+import org.springframework.stereotype.Component
+
+import javax.servlet.Filter
+import javax.servlet.FilterChain
+import javax.servlet.FilterConfig
+import javax.servlet.ServletException
+import javax.servlet.ServletRequest
+import javax.servlet.ServletResponse
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+/**
+ * Created by xieyuanliang on 15-4-14.
+ */
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+class CorsFilter implements Filter {
+
+    @Override
+    void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    void doFilter(ServletRequest req, ServletResponse res,
+                  FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req
+        HttpServletResponse response = (HttpServletResponse) res
+        response.setHeader("Access-Control-Allow-Origin", "*")
+        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE")
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with")
+        response.setHeader("Access-Control-Max-Age", "3600")
+
+        if (request.getMethod() != 'OPTIONS') {
+            filterChain.doFilter(req, res)
+        }
+    }
+
+    @Override
+    void destroy() {
+
+    }
+}
